4891691d611501dafa70b1775e188c23b36a8c2f
Anna Shcherbakova <ShcherbakovaAnna90@yandex.ru>
2023-11-08 18:16:49 +0600

    Added commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int initialAccount = 500;
+        int replenishAccount = 1300;
+        int account = initialAccount + replenishAccount;
+        int bonus = replenishAccount / 100;
+        int finalAccount = initialAccount + replenishAccount + bonus;
+
+        if (replenishAccount > 1000) {
+            System.out.println("Ваш баланс" + " " + finalAccount + " " + "Количество бонусов" + " " + bonus);
+        } else {
+            System.out.println("Ваш баланс" + " " + account );
+        }
     }
 }
